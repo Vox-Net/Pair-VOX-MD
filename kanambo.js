458177109ddc9579e4b11bb5415a9f6c15a50b6d
Vox-Net <voxnet2025@gmail.com>
2025-03-05 19:43:56 +0300

    Update kanambo.js

--- kanambo.js
+++ kanambo.js
@@ -3,16 +3,11 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./kanamboqr'),
-    code = require('./pair');
+let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/kanamboqr', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/kanambopage.html')
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
