67fa9609a5baf5ab644b2aca7b3b17bde228106d
Vox-Net <voxnet2025@gmail.com>
2025-03-05 21:34:59 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -22,11 +22,11 @@ router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
 
-    async function WASI_MD_PAIR_CODE() {
+    async function VOX_MD_PAIR_CODE() {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
 
         try {
-            let Pair_Code_By_Wasi_Tech = Wasi_Tech({
+            let Pair_Code_By_VOX_Tech = Wasi_Tech({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
@@ -36,17 +36,17 @@ router.get('/', async (req, res) => {
                 browser: ["Chrome (Linux)", "", ""]
             });
 
-            if (!Pair_Code_By_Wasi_Tech.authState.creds.registered) {
+            if (!Pair_Code_By_VOX_Tech.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const code = await Pair_Code_By_Wasi_Tech.requestPairingCode(num);
+                const code = await Pair_Code_By_VOX_Tech.requestPairingCode(num);
                 if (!res.headersSent) {
                     await res.send({ code });
                 }
             }
 
-            Pair_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds);
-            Pair_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
+            Pair_Code_By_VOX_Tech.ev.on('creds.update', saveCreds);
+            Pair_Code_By_VOX_Tech.ev.on("connection.update", async (s) => {
                 const { connection, lastDisconnect } = s;
 
                 if (connection == "open") {
@@ -54,9 +54,9 @@ router.get('/', async (req, res) => {
                     let data = fs.readFileSync(`${__dirname}/temp/${id}/creds.json`);
                     await delay(800);
                     let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Wasi_Tech.sendMessage(Pair_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
+                    let session = await Pair_Code_By_VOX_Tech.sendMessage(Pair_Code_By_VOX_Tech.user.id, { text: '' + b64data });
 
-                    let WASI_MD_TEXT = `
+                    let VOX_MD_TEXT = `
 ╔════════════════════════╗
 ║   🌟 *SESSION CONNECTED* 🌟   
 ╚════════════════════════╝
@@ -72,10 +72,10 @@ router.get('/', async (req, res) => {
   🚀 *Powered by ©VOXNET.INC*  
 ╚════════════════════════╝`;
 
-                    await Pair_Code_By_Wasi_Tech.sendMessage(Pair_Code_By_Wasi_Tech.user.id, { text: WASI_MD_TEXT }, { quoted: session });
+                    await Pair_Code_By_VOX_Tech.sendMessage(Pair_Code_By_VOX_Tech.user.id, { text: VOX_MD_TEXT }, { quoted: session });
 
                     await delay(100);
-                    await Pair_Code_By_Wasi_Tech.ws.close();
+                    await Pair_Code_By_VOX_Tech.ws.close();
                     await removeFile('./temp/' + id);
 
                     console.log("✅ Session connected. Restarting server...");
@@ -83,7 +83,7 @@ router.get('/', async (req, res) => {
                     process.exit(1);  // 🔄 Restart the server
                 } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    WASI_MD_PAIR_CODE();
+                    VOX_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -95,7 +95,7 @@ router.get('/', async (req, res) => {
         }
     }
 
-    return await WASI_MD_PAIR_CODE();
+    return await VOX_MD_PAIR_CODE();
 });
 
 module.exports = router;
