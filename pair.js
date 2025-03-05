8188b15a5ba7aef208d692c927bc0b5673b5bf4b
Vox-Net <voxnet2025@gmail.com>
2025-03-05 21:38:54 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
 
         try {
-            let Pair_Code_By_VOX_Tech = Wasi_Tech({
+            let Pair_Code_By_VOX_Tech = VOX_Tech({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
