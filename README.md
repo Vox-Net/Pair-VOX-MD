3e0d8fbdad881468850c060642326a6475871d03
Vox-Net <voxnet2025@gmail.com>
2025-03-05 19:40:56 +0300

    Voxmd ⚒️

---
+++ README.md
@@ -0,0 +1,22 @@
+# KANAMBO - Generator
+- Kindly star my repo
+- Fork and edit as you wish
+- Deploy to your favourite hosting server eg Heroku or Render or self hosting
+
+<strong>NB:<strong/> This repo also generates session ID for all bots using whiskeysockets/baileys
+
+<br/>QR- WEB - PAIR CODE FOR BOT WITH WHISKEYSOCKETS/BAILEYS
+
+
+
+
+[`ℹ️Contact Owner`](https://wa.me/254114148625)
+
+FORK THE REPOSITORY (Repo) 
+    <br>
+<a href="https://github.com/Kanambp/KANAMBO-QR"><img title="WEB" src="https://img.shields.io/badge/FORK *KANAMBO*-QR?color=black&style=for-the-badge&logo=stackshare"></a>
+
+Now Deploy
+    <br>
+<a href='https://dashboard.heroku.com/new?template=https://github.com/Kanambp/KANAMBO-QR' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-black?style=for-the-badge&logo=heroku&logoColor=white'/>
+
---
+++ pair.js
@@ -0,0 +1,97 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Wasi_Tech,    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function WASI_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_Wasi_Tech = Wasi_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "", ""]
+             });
+             if(!Pair_Code_By_Wasi_Tech.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Wasi_Tech.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Wasi_Tech.sendMessage(Pair_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
+
+               let WASI_MD_TEXT = `
+*_Session Connected By KANAMBO 👌_*
+*_Made With 🤍_*
+______________________________________
+╔════◇
+║ *『AMAZING YOU'VE CHOSEN KANAMBO MD』*
+║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+╚════════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Owner:* _https://wa.me/+254114148625_
+║❒ *Repo:* _https://github.com/Kanambp/dreaded-v2_
+║❒ *WaGroup:* _https://chat.whatsapp.com/FF6YuOZTAVB6Lu65cnY5BN_ 
+╚════════════════════════╝
+*_Am glad you have chosen KANAMBO TECH please 🙏 don't share the session_*
+_____________________________________
+	
+_Don't Forget To Give Star To My Repo_`
+ await Pair_Code_By_Wasi_Tech.sendMessage(Pair_Code_By_Wasi_Tech.user.id,{text:WASI_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Wasi_Tech.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    WASI_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await WASI_MD_PAIR_CODE()
+});
+module.exports = router
