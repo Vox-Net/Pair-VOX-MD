2d3b22dcb99f437572e2e53f84b68bff1dbc5db3
Vox-Net <voxnet2025@gmail.com>
2025-03-24 16:00:20 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,17 +1,11 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
-const { makeid } = require('./id');
-const QRCode = require('qrcode');
 const express = require('express');
 const fs = require('fs');
-let router = express.Router();
+const QRCode = require('qrcode');
 const pino = require("pino");
-const {
-    default: Kanambo_Tech,
-    useMultiFileAuthState,
-    Browsers,
-    delay,
-} = require("@whiskeysockets/baileys");
+const { default: Kanambo_Tech, useMultiFileAuthState, Browsers, delay } = require("@whiskeysockets/baileys");
+const { makeid } = require('./id');
+
+let router = express.Router();
 
 function removeFile(FilePath) {
     if (fs.existsSync(FilePath)) {
@@ -19,8 +13,36 @@ function removeFile(FilePath) {
     }
 }
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+                <title>QR Code Login</title>
+                <script>
+                    function refreshQR() {
+                        fetch('/get-qrcode')
+                            .then(response => response.blob())
+                            .then(blob => {
+                                const url = URL.createObjectURL(blob);
+                                document.getElementById('qrcode').src = url;
+                            })
+                            .catch(error => console.error('Error refreshing QR:', error));
+                    }
+                </script>
+            </head>
+            <body>
+                <h1>Scan QR Code to Login</h1>
+                <img id="qrcode" src="/get-qrcode" alt="QR Code" width="300" height="300"/>
+                <br/>
+                <button onclick="refreshQR()">Refresh QR Code</button>
+            </body>
+        </html>
+    `);
+});
+
+router.get('/get-qrcode', async (req, res) => {
     const id = makeid();
+    
     async function KANAMBO_MD_QR_CODE() {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
 
@@ -37,14 +59,15 @@ router.get('/', async (req, res) => {
                 const { connection, lastDisconnect, qr } = s;
 
                 if (qr) {
-                    await res.end(await QRCode.toBuffer(qr));
+                    res.setHeader('Content-Type', 'image/png');
+                    res.end(await QRCode.toBuffer(qr));
                     console.log("QR Code Generated - Scan Now!");
                 }
 
                 if (connection === "open") {
                     console.log("Successfully logged in!");
                     await delay(2000);
-                    
+
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     let b64data = Buffer.from(data).toString('base64');
                     let session = await Qr_Code_By_Kanambo_Tech.sendMessage(Qr_Code_By_Kanambo_Tech.user.id, { text: b64data });
